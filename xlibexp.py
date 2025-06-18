ec0c2f15ed78d74d3704b32f122eac4dd680c0cd
samejima-san <samejimasama@gmail.com>
2025-06-18 04:07:18 -0400

    started xbox exporter, gets stuff but doesnt add to db yet

---
+++ xlibexp.py
@@ -0,0 +1,52 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+
+gamertag = "Saint-3114"
+gamerid = "439884"
+url = f"https://www.trueachievements.com/gamer/{gamertag}/games"
+counter = 1
+
+url3 = (
+    f"https://www.trueachievements.com/gamer/{gamertag}/games?"
+    "function=AjaxList"
+    "&params=oGamerGamesList|"
+    f"&oGamerGamesList_Page={counter}"
+    "&oGamerGamesList_ItemsPerPage=400"
+    "oGamerGamesList_ShowAll=True"
+)
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64"
+}
+responselist = []
+urllist = []
+games = []
+puregamelist = []
+
+def getListOfGames():
+    response = requests.get(url3, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    end = soup.find(class_='l').a.text.strip()
+    end = int(end)
+    for counter in range(1, end+1):
+        urllist.append(
+            f"https://www.trueachievements.com/gamer/{gamertag}/games?function=AjaxList&params=oGamerGamesList|&oGamerGamesList_Page={counter}&oGamerGamesList_ItemsPerPage=400oGamerGamesList_ShowAll=True"
+        )
+
+    for url in urllist:
+        response = requests.get(url, headers=headers)
+        soup = BeautifulSoup(response.text, "html.parser")
+        responselist.append(soup)
+    
+    for response in responselist:
+        games.append(response.find_all(class_='smallgame'))
+
+    for game in games:
+        for arr in game:
+            puregamelist.append((arr.text, arr.find("a").get("href", "").strip()))
+
+    return puregamelist
+
+
